a0b8100a44e3854bd559e1e5f8cc9efd082328f2
Nipun <nipun@Admins-MacBook-Pro-3.local>
2019-02-24 13:18:06 -0800

    ColorSwitch Game

---
+++ ColorSwitch/Templete/Setting.swift
@@ -0,0 +1,21 @@
+//
+//  Setting.swift
+//  ColorSwitch
+//
+//  Created by Nipun Garg on 9/13/18.
+//  Copyright © 2018 Nipun Garg. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1           //01
+    static let switchCategory: UInt32 = 0x1 << 1    //10
+}
+
+enum ZPositions {
+    static let label: CGFloat = 0
+    static let ball: CGFloat = 1
+    static let colorSwitch: CGFloat = 2
+}
---
+++ ColorSwitch/ViewController/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  ColorSwitch
+//
+//  Created by Nipun Garg on 9/13/18.
+//  Copyright © 2018 Nipun Garg. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = MenuScene(size: view.bounds.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
